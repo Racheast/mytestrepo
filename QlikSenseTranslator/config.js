6c98604c2a3fa2b46c4573d472a9225c06455093
Alexander Eigner <alexander.eigner@gmm.local>
2017-10-23 16:38:42 +0200

    small changes in target file path and source file path.

--- QlikSenseTranslator/config.js
+++ QlikSenseTranslator/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  source_app_filepath : "\\\\gmm.local\\Daten\\Roaming$\\alexander.eigner\\Eigene Dateien\\Qlik\\Sense\\Apps\\MultiLang_TestApp.qvf",
+  source_app_filepath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\MultiLang_TestApp.qvf",
   csv_filepath : "./csv/translations.csv",
-  qvf_dir : "\\\\gmm.local\\Daten\\Roaming$\\alexander.eigner\\Eigene Dateien\\Qlik\\Sense\\Apps\\",
+  qvf_dir : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\",
   qlik_engine_url : "ws://localhost:4848/app/engineData",
   qlik_targetApp_sheet : 'd6e0c807-e9d5-4d38-bae9-76a4d665dc3e',
   getTargetAppFileName: function(lang){
