f68e9ecdfdc8c748a9ef65e19c950b47ab800747
Alexander Eigner <alexander.eigner@gmm.local>
2017-10-24 10:12:49 +0200

    Implemented first version of translation of Uebersetzung-App. Added some sample translations to dictionary. Small changes in config.js

--- QlikSenseTranslator/config.js
+++ QlikSenseTranslator/config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  source_app_filepath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\MultiLang_TestApp.qvf",
+  //source_app_filepath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\MultiLang_TestApp.qvf",
+  source_app_filepath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\Übersetzungs-App_original - Kopie.qvf",
   csv_filepath : "./csv/translations.csv",
-  qvf_dir : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\",
+  target_app_dirpath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\",
   qlik_engine_url : "ws://localhost:4848/app/engineData",
-  qlik_targetApp_sheet : 'd6e0c807-e9d5-4d38-bae9-76a4d665dc3e',
+  //qlik_targetApp_sheet : 'd6e0c807-e9d5-4d38-bae9-76a4d665dc3e',
+  qlik_targetApp_sheet : "jNdRe",
+  
   getTargetAppFileName: function(lang){
-	  var target_app_name = "MyTestApp";
+	  var target_app_name = "Übersetzungs-App";
 	  return target_app_name + "_" + lang + ".qvf";
+  },
+  
+  getTargetAppFilePath: function(lang){
+	  return this.target_app_dirpath + this.getTargetAppFileName(lang);
   }
 };
