40b1ef5ae6608d90fdabfa0f7cf8d5ae0d073669
Alexander Eigner <alexander.eigner@gmm.local>
2017-10-25 10:10:41 +0200

    Implemented async-await functionality in QlikSenseTranslator.js. First test was OK.

--- QlikSenseTranslator/QlikSenseTranslator.js
+++ QlikSenseTranslator/QlikSenseTranslator.js
@@ -27,7 +27,7 @@ if(process.argv.length == 3 ){
 	langChoice = process.argv[2];
 
 	if(langArgs.indexOf(langChoice)>-1){		
-		start();
+		startApp();
 	}else{
 		console.log("Invalid arguments! Please use one of the following arguments: " + langArgs + " .");
 	}
@@ -35,23 +35,7 @@ if(process.argv.length == 3 ){
 	console.log("Invalid command format! Please use the following format: node QlikSenseTranslator.js <language>");
 }
 
-function resolveAfter2Seconds(x) { 
-  return new Promise(resolve => {
-    resolve(x)
-  });
-}
-
-async function f1() {
-  var x = await resolveAfter2Seconds(10);
-  console.log(x); // 10
-}
-async function f2() {
-	console.log("starting f2");
-	var sheetIds = await getSheets();
-	console.log("*** *** await finished. sheetIds: " + sheetIds);
-}
-
-function start(){   
+function startApp(){   
 	csv
 	 .fromPath(config.csv_filepath)
 	 .on("data", function(data){
@@ -68,32 +52,54 @@ function start(){ 
 		var streams = fs.createReadStream(config.source_app_filepath).pipe(fs.createWriteStream(config.target_app_dirpath + config.getTargetAppFileName(langChoice)));
 	 	streams.on('finish', function () {
 			console.log("Copying finished.\n");
-			//var sheetIds = await getSheets();
-			//translateSheets(session,global,app,sheetIds);
-			//f1();
-			f2();
+			initTranslation();
 		});
 	})
 }
 
-function getSheets(){
+async function initTranslation() {
+	console.log("starting f2");
+	var global = await openConnection(session);
+	var app = await getApp(global);
+	var sheetIds = await getAllSheets(app);
+	
+	for(var i=0; i<sheetIds.length; i++){
+		await translateSheet(app,sheetIds[i]);
+	}
+	
+	console.log("Closing the session ...");
+	
+	session.close();
+}
+
+function openConnection(s){
+	return new Promise(resolve => {
+		console.log("Starting openConnection ...");
+		s.open()
+		.then((global)=>{
+			console.log("Connection successful.\nReturning global object ...");
+			resolve(global);
+		})
+	});
+}
+
+function getApp(global){
+	return new Promise(resolve => {
+		console.log("Starting getApp ...");
+		global.openDoc(config.getTargetAppFilePath(langChoice), '', '', '', false)
+		.then((app) => {
+			console.log("App received.\nReturning app object ...");
+			resolve(app);
+		})
+	});
+}
+
+function getAllSheets(app){
 	return new Promise(resolve => {
 		console.log("Starting getAllSheets() ...");
-		
+		console.log("Trying to create sheetlist ...");
 		var sheetIds = [];
-		var app, global;
-		session.open()
-		.then((global_received) => {
-			console.log("Connection successful.");
-			global = global_received;
-			return global.openDoc(config.getTargetAppFilePath(langChoice), '', '', '', false);
-		})
-		.then((app_received) => {
-			console.log("Target app opened successfully.");
-			console.log("Trying to create sheetlist ... ");
-			app = app_received;
-			return app.createSessionObject(functions.getSheetListProperties());
-		})
+		app.createSessionObject(functions.getSheetListProperties())
 		.then((sheetList) => {
 			console.log("Sheet list received ...");
 			console.log("Trying to get layout from sheet list ...");
@@ -108,15 +114,85 @@ function getSheets(){
 		.then(() => {
 			console.log("getAllSheets() finished.\n");
 			resolve(sheetIds);
-			session.close();
 		})
 		.catch(function (error) {
 				console.log(error);
-				console.log("getSheetIds has not finished successfully!\nClosing the session ...\n");
+				console.log("getSheetIds has not finished successfully!");
+				resolve(sheetIds);
+		});
+	});
+}
+
+function translateSheet(app,sheetId) {
+	return new Promise(resolve => {
+		console.log("Starting translateSheets() for sheetId " + sheetId + " ... ");
+
+		var objects = [];  //needed for further processing
+		var dimensions = [];  //needed for further processing
+		
+		console.log("Trying to get sheet with id " + sheetId);
+		app.getObject({qId:sheetId})
+		.then(function (sheet) {  //sheet
+				console.log("Sheet " + sheet.id + " received.");
+				console.log("Trying to getLayout() from sheet ...");
+				return sheet.getLayout();
+		}).then(function(layout) {
+				console.log("Layout received");
+				console.log("Trying to get all objects from layout ...");
+				return Promise.all(functions.getAllObjectsFromLayout(app, layout));
+		}).then((objects_received) => {
+				console.log("All objects received.");
+				console.log("Trying to getProperties() of all objects");
+				return Promise.all(functions.getPropertiesForAllObjects(objects,objects_received));
+		})
+		.then((propertiesArray) => {
+			console.log("All properties received.");
+			console.log("Trying to apply object patches ...");
+			return Promise.all(functions.getApplyPatchesTasksForObjects(objects,propertiesArray,dictionary));
+		})
+		.then(() => {
+			console.log("Trying to create dimension list ...");
+			return app.createSessionObject(functions.getDimensionListProperties());
+		})
+		.then((dimensionList) => {
+			console.log("Dimension list created.");
+			console.log("Trying to getLayout() from dimension list ...");
+			return dimensionList.getLayout();
+		})
+		.then((layout) => {
+			console.log("Layout received.");
+			console.log("Trying to get all dimensions ...");
+			return Promise.all(functions.getDimensionsFromDimensionListLayout(app,layout));
+		})
+		
+		.then((dimensions_received) => {
+			console.log("All dimensions received.");
+			console.log("Trying to getProperties() for all dimensions ...");
+			return Promise.all(functions.getPropertiesForAllDimensions(dimensions, dimensions_received));
+		})
+		.then((propertiesArray) => {
+			console.log("All properties received.");
+			console.log("Trying to apply dimension patches ...");
+			return Promise.all(functions.getApplyPatchesTasksForDimensions(dimensions,propertiesArray,dictionary));
+		})
+
+		.then(() => {
+			console.log("Trying doSave() ...");
+			return app.doSave();
+		})
+		.then(() => {
+			resolve();
+		})
+		.catch(function (error) {
+				console.log(error);
+				console.log("Translation has not finished successfully!\nClosing the session ...\n");
 				session.close();
 		});
 	});
 }
+
+
+// old code //////////////
 
 /*
 function getAllSheets(){
@@ -163,6 +239,7 @@ function getAllSheets(){
 }
 */
 
+/*
 function translateSheets(session,global,app,sheetIds) {
 	var sheetId = sheetIds.pop();
 	console.log("Starting translateSheets() for sheetId " + sheetId + " ... ");
@@ -235,3 +312,4 @@ function translateSheets(session,global,app,sheetIds) {
 			session.close();
     });
 }
+*/
