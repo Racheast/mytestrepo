5d56653c6357856ebcd0973db991fea49036e620
Alexander Eigner <alexander.eigner@gmm.local>
2017-10-25 11:41:51 +0200

    Added delimiter=';' option to fast-csv. Added Peter's translation-csv to csv folder and updated csv-path in config.js.

--- QlikSenseTranslator/QlikSenseTranslator.js
+++ QlikSenseTranslator/QlikSenseTranslator.js
@@ -3,15 +3,12 @@ const WebSocket = require('ws');
 const path = require('path');
 const fs = require('fs');
 const schema = require('enigma.js/schemas/12.20.0.json');
-
 var config = require('./config');
 var functions = require('./functions');
 var csv = require("fast-csv");
-
 var dictionary = [];
 const langArgs = ["DE"];
 var langChoice;
-
 const session = enigma.create({
   schema,
   url: config.qlik_engine_url, 
@@ -41,21 +38,17 @@ async function start() {
 	await copyApp();
 	var global = await openConnection(session);
 	var app = await getApp(global);
-	var sheetIds = await getAllSheets(app);
-	
+	var sheetIds = await getAllSheetIds(app);
 	for(var i=0; i<sheetIds.length; i++){
 		await translateSheet(app,sheetIds[i]);
 	}
-	
 	console.log("Closing the session ...");
-	
 	session.close();
 }
 
 function copyApp(){
 	return new Promise(resolve => {
 		console.log("Copying " + config.source_app_filepath + " into " + config.target_app_dirpath + config.getTargetAppFileName(langChoice) + " ...");
-		
 		var streams = fs.createReadStream(config.source_app_filepath).pipe(fs.createWriteStream(config.target_app_dirpath + config.getTargetAppFileName(langChoice)));
 	 	streams.on('finish', function () {
 			console.log("Copying finished.\n");
@@ -67,7 +60,7 @@ function copyApp(){
 function writeDictionary(){
 	return new Promise(resolve => {
 		csv
-		 .fromPath(config.csv_filepath)
+		 .fromPath(config.csv_filepath, {delimiter: ';'})
 		 .on("data", function(data){
 			if(langChoice == "DE"){
 				console.log("Writing dictionary: [" + data[0] + "]");
@@ -75,7 +68,7 @@ function writeDictionary(){
 			}
 			
 		 })
-		 .on("end", function(){  	//initiate subsequent translation logic here
+		 .on("end", function(){  	
 			console.log("Writing dictionary: finished");
 			resolve();
 		})
@@ -104,9 +97,9 @@ function getApp(global){
 	});
 }
 
-function getAllSheets(app){
+function getAllSheetIds(app){
 	return new Promise(resolve => {
-		console.log("Starting getAllSheets() ...");
+		console.log("Starting getAllSheetIds() ...");
 		console.log("Trying to create sheetlist ...");
 		var sheetIds = [];
 		app.createSessionObject(functions.getSheetListProperties())
@@ -200,126 +193,3 @@ function translateSheet(app,sheetId) {
 		});
 	});
 }
-
-
-// old code //////////////
-
-/*
-function getAllSheets(){
-	console.log("Starting getAllSheets() ...");
-	var sheetIds = [];
-	var app, global;
-	session.open()
-	.then((global_received) => {
-		console.log("Connection successful.");
-		global = global_received;
-		return global.openDoc(config.getTargetAppFilePath(langChoice), '', '', '', false);
-	})
-	.then((app_received) => {
-		console.log("Target app opened successfully.");
-		console.log("Trying to create sheetlist ... ");
-		app = app_received;
-		return app.createSessionObject(functions.getSheetListProperties());
-	})
-	.then((sheetList) => {
-		console.log("Sheet list received ...");
-		console.log("Trying to get layout from sheet list ...");
-		return sheetList.getLayout();
-	})
-	.then((layout) => {
-		for(var i=0; i<layout.qAppObjectList.qItems.length; i++){
-			var qItem = layout.qAppObjectList.qItems[i];
-			sheetIds.push(qItem.qInfo.qId);
-		}
-	}).then(() => {
-		console.log("getAllSheets() finished.\n");
-		
-		if(sheetIds.length > 0){
-			translateSheets(session,global,app,sheetIds);
-		}else{
-			console.log("No sheets were found in the app.\nClosing the session ...\n");
-			session.close();
-		}
-	})
-	.catch(function (error) {
-            console.log(error);
-			console.log("getSheetIds has not finished successfully!\nClosing the session ...\n");
-			session.close();
-    });
-}
-*/
-
-/*
-function translateSheets(session,global,app,sheetIds) {
-	var sheetId = sheetIds.pop();
-	console.log("Starting translateSheets() for sheetId " + sheetId + " ... ");
-	console.log("Trying to connect to the engine at " + config.qlik_engine_url + " ...");
-
-	var objects = [];  //needed for further processing
-	var dimensions = [];  //needed for further processing
-	
-	
-	app.getObject({qId:sheetId})
-	.then(function (sheet) {  //sheet
-            console.log("Sheet " + sheet.id + " received.");
-            console.log("Trying to getLayout() from sheet ...");
-			return sheet.getLayout();
-    }).then(function(layout) {
-			console.log("Layout received");
-			console.log("Trying to get all objects from layout ...");
-			return Promise.all(functions.getAllObjectsFromLayout(app, layout));
-	}).then((objects_received) => {
-			console.log("All objects received.");
-			console.log("Trying to getProperties() of all objects");
-			return Promise.all(functions.getPropertiesForAllObjects(objects,objects_received));
-	})
-	.then((propertiesArray) => {
-		console.log("All properties received.");
-		console.log("Trying to apply object patches ...");
-		return Promise.all(functions.getApplyPatchesTasksForObjects(objects,propertiesArray,dictionary));
-	})
-	.then(() => {
-		console.log("Trying to create dimension list ...");
-		return app.createSessionObject(functions.getDimensionListProperties());
-	})
-	.then((dimensionList) => {
-		console.log("Dimension list created.");
-		console.log("Trying to getLayout() from dimension list ...");
-		return dimensionList.getLayout();
-	})
-	.then((layout) => {
-		console.log("Layout received.");
-		console.log("Trying to get all dimensions ...");
-		return Promise.all(functions.getDimensionsFromDimensionListLayout(app,layout));
-	})
-	
-	.then((dimensions_received) => {
-		console.log("All dimensions received.");
-		console.log("Trying to getProperties() for all dimensions ...");
-		return Promise.all(functions.getPropertiesForAllDimensions(dimensions, dimensions_received));
-	})
-	.then((propertiesArray) => {
-		console.log("All properties received.");
-		console.log("Trying to apply dimension patches ...");
-		return Promise.all(functions.getApplyPatchesTasksForDimensions(dimensions,propertiesArray,dictionary));
-	})
-
-	.then(() => {
-		console.log("Trying doSave() ...");
-		return app.doSave();
-	})
-	.then(() => {
-		if(sheetIds.length>0){
-			translateSheets(session,global,app,sheetIds);
-		}else{
-			console.log("Closing the session ... \n");
-			session.close();
-		}
-	})
-	.catch(function (error) {
-            console.log(error);
-			console.log("Translation has not finished successfully!\nClosing the session ...\n");
-			session.close();
-    });
-}
-*/
--- QlikSenseTranslator/config.js
+++ QlikSenseTranslator/config.js
@@ -1,11 +1,12 @@
 module.exports = {
   //source_app_filepath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\MultiLang_TestApp.qvf",
-  source_app_filepath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\Übersetzungs-App_original - Kopie.qvf",
-  csv_filepath : "./csv/translations.csv",
+  source_app_filepath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\Übersetzungs-App.qvf",
+  //csv_filepath : "./csv/translations.csv",
+  csv_filepath : "./csv/label_liste_vorläufig.csv",
   target_app_dirpath : "C:\\Users\\Alex\\Documents\\Qlik\\Sense\\Apps\\",
   qlik_engine_url : "ws://localhost:4848/app/engineData",
   //qlik_targetApp_sheet : 'd6e0c807-e9d5-4d38-bae9-76a4d665dc3e',
-  qlik_targetApp_sheet : "jNdRe",
+  //qlik_targetApp_sheet : "jNdRe",
   
   getTargetAppFileName: function(lang){
 	  var target_app_name = "Übersetzungs-App";
